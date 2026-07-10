d313444bade3da842845a0d0e6dc5a5b17ea9afe
yuliyaBarashka <zohn12333@gmail.com>
2026-07-10 08:00:59 +0700

    *

--- myshop/catalog/urls.py
+++ myshop/catalog/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('catalog/', views.catalog, name='catalog'),
     path('contacts/', views.contacts, name='contacts'),
-    path('product/<int:pk>/', views.product_detail, name='product_detail'),
+    path('product/<int:pk>/', views.product_detail, name='product_detail'),  # ✅ /product/int:pk/
+    path('add/', views.add_product, name='add_product'),  # Доп. задание
 ]
