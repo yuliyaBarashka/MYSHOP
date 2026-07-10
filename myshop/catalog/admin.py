fd3010593c30a93ead29999bc558fbe4621cae63
yuliyaBarashka <zohn12333@gmail.com>
2026-07-10 07:59:38 +0700

    admin.py

--- myshop/catalog/admin.py
+++ myshop/catalog/admin.py
@@ -1,20 +1,12 @@
 from django.contrib import admin
-from .models import Category, Product, Contact
+from .models import Category, Product
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['id', 'name']
-    search_fields = ['name']
-    ordering = ['name']
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['id', 'name', 'price', 'category', 'created_at']
+    list_display = ['id', 'name', 'price', 'category']
     list_filter = ['category']
     search_fields = ['name', 'description']
-    list_editable = ['price']
-    ordering = ['-created_at']
-
-@admin.register(Contact)
-class ContactAdmin(admin.ModelAdmin):
-    list_display = ['phone', 'email', 'address', 'work_hours']
