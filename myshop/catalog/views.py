a25ee29ed7c96a77a1091617f676588e1bd4fe33
iuliia <zohn12333@gmail.com>
2026-07-31 21:15:22 +0700

    - Update ProductDetailView to use cached product data - Create CategoryProductsView for category filtering - Add cache clearing on product update/delete/moderate - Pass categories context to CatalogView - Use service methods instead of direct ORM queries"

--- myshop/catalog/views.py
+++ myshop/catalog/views.py
@@ -2,9 +2,10 @@ from django.views.generic import ListView, DetailView, CreateView, UpdateView, D
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.core.exceptions import PermissionDenied
-from django.shortcuts import get_object_or_404
+from django.shortcuts import render
 from .models import Product, Category
 from .forms import ProductForm
+from .services import ProductService
 
 
 class HomeView(ListView):
@@ -14,7 +15,6 @@ class HomeView(ListView):
     paginate_by = 3
 
     def get_queryset(self):
-        # Показываем только опубликованные продукты
         return Product.objects.filter(is_published=True).order_by('-created_at')
 
 
@@ -24,8 +24,13 @@ class CatalogView(ListView):
     context_object_name = 'products'
 
     def get_queryset(self):
-        # Показываем только опубликованные продукты
         return Product.objects.filter(is_published=True).order_by('-created_at')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Добавляем категории в контекст
+        context['categories'] = Category.objects.all()
+        return context
 
 
 class ProductDetailView(DetailView):
@@ -33,6 +38,13 @@ class ProductDetailView(DetailView):
     template_name = 'catalog/product_detail.html'
     context_object_name = 'product'
     pk_url_kwarg = 'pk'
+
+    def get_object(self, queryset=None):
+        # Используем сервис для получения продукта с кешированием
+        product = ProductService.get_product_detail(self.kwargs.get('pk'))
+        if product is None:
+            raise PermissionDenied("Продукт не найден")
+        return product
 
 
 class ContactsView(TemplateView):
@@ -46,7 +58,6 @@ class ProductCreateView(LoginRequiredMixin, CreateView):
     success_url = reverse_lazy('catalog:catalog')
 
     def form_valid(self, form):
-        # Автоматически привязываем владельца
         form.instance.owner = self.request.user
         return super().form_valid(form)
 
@@ -68,10 +79,14 @@ class ProductUpdateView(LoginRequiredMixin, UpdateView):
         obj = super().get_object(queryset)
         user = self.request.user
 
-        # Проверяем права: владелец или модератор может редактировать
         if obj.owner == user or user.has_perm('catalog.can_unpublish_product'):
             return obj
         raise PermissionDenied("У вас нет прав на редактирование этого продукта")
+
+    def form_valid(self, form):
+        # Очищаем кеш при обновлении
+        ProductService.clear_product_cache(self.object.id)
+        return super().form_valid(form)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -90,8 +105,9 @@ class ProductDeleteView(LoginRequiredMixin, DeleteView):
         obj = super().get_object(queryset)
         user = self.request.user
 
-        # Проверяем права: владелец или модератор может удалить
         if obj.owner == user or user.has_perm('catalog.can_unpublish_product'):
+            # Очищаем кеш при удалении
+            ProductService.clear_product_cache(obj.id)
             return obj
         raise PermissionDenied("У вас нет прав на удаление этого продукта")
 
@@ -108,9 +124,33 @@ class ProductModerateView(LoginRequiredMixin, PermissionRequiredMixin, UpdateVie
     permission_required = 'catalog.can_unpublish_product'
 
     def get_success_url(self):
+        # Очищаем кеш после модерации
+        ProductService.clear_product_cache(self.object.id)
         return reverse_lazy('catalog:catalog')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Модерация товара'
+        return context
+
+
+# Новое представление для отображения продуктов по категории
+class CategoryProductsView(ListView):
+    model = Product
+    template_name = 'catalog/category_products.html'
+    context_object_name = 'products'
+
+    def get_queryset(self):
+        category_id = self.kwargs.get('category_id')
+        # Используем сервис для получения продуктов с кешированием
+        return ProductService.get_products_by_category(category_id)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        category_id = self.kwargs.get('category_id')
+        try:
+            category = Category.objects.get(id=category_id)
+            context['category'] = category
+        except Category.DoesNotExist:
+            context['category'] = None
         return context
