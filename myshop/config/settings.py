e936e65d83cd23b0ed44250d4c5299fab9315f32
iuliia <zohn12333@gmail.com>
2026-07-31 21:14:27 +0700

    - Install django-redis and redis packages - Configure Redis as default cache backend - Add cache key prefix 'myshop' - Set Redis connection to localhost:6379/1 - Update requirements.txt with new dependencies"

--- myshop/config/settings.py
+++ myshop/config/settings.py
@@ -68,6 +68,19 @@ LOGIN_URL = 'users:login'
 LOGIN_REDIRECT_URL = 'catalog:home'
 LOGOUT_REDIRECT_URL = 'catalog:home'
 
+# Настройки кеширования Redis
+# Настройки кеширования Redis (упрощенная версия)
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379/1',
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        },
+        'KEY_PREFIX': 'myshop',
+    }
+}
+
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
     {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
